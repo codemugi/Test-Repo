4aa0db4a56bb106e7ba1603f9c2dbb4e0282d254
Shanmugam Pannalal <MagIMAC@MUGIMAC.local>
2019-06-22 12:00:12 -0600

    removed some code

--- pysys_version.py
+++ pysys_version.py
@@ -1,15 +1,4 @@
-import math
-import sys
-from os import rename
 import requests
-
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(Who_to_greet):
-    greeting = "Hello, {}".format(Who_to_greet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
