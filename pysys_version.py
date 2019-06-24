c8fe3fa6d5e5b2f13a159f6026d2ef2dbf49edb1
codemugi <codemugi@focuslight.ca>
2019-06-23 22:50:20 -0600

    update

--- pysys_version.py
+++ pysys_version.py
@@ -1,5 +1,16 @@
+import math
+import sys
+
 import requests
 
-
+print (sys.version)
+print(sys.executable)
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+print(greet("World"))
+print(greet("Jo"))
